394276997e46399ffb38f7be4d312505b7c3e62b
Manuel <manuel.aveta2001@gmail.com>
2025-05-12 15:07:24 +0200

    added dark theme

---
+++ README.md
@@ -0,0 +1,5 @@
+Project Name: Job Tracker
+Tach: React + TS, Tailwind, Firebase Auth, Firebase DB
+
+Tailwind Responsive Design Sizes: md:, lg:.
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import './App.css'
 import Header from './components/header/Header'
 
 function App() {
-
   return (
-    <>
-      <Header />
-    </>
+    <Header />
   )
 }
 
